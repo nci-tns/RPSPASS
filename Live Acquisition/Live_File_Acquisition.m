327d0daed130d6369d6c873c4cacf463d42d7d02
Joshua Welsh <33503849+joadwe@users.noreply.github.com>
2022-04-24 04:32:46 -0400

    live acquisition interface fully working

--- Live Acquisition/Live_File_Acquisition.m
+++ Live Acquisition/Live_File_Acquisition.m
@@ -3,30 +3,34 @@ function [status, message]=Live_File_Acquisition(app)
 filepath = getprefRPSPASS('RPSPASS','acquisition_dir');
 filelocator = getprefRPSPASS('RPSPASS','filelocatorSelected');
 
-while app.LiveAcquisition == true
-    % if the filepath exists
-    if ~isequal(filepath,0)
+% if the filepath exists
+if ~isequal(filepath,0)
 
-        % load .h5 combined files only
-        SelectedFolderInfo = dir(fullfile(filepath,'*.h5'));
-        Filenames = {SelectedFolderInfo.name}';
-        [Filenames, FileGroup, FileNo] = ObtainFilenames(Filenames, filelocator);
+    % load .h5 combined files only
+    SelectedFolderInfo = dir(fullfile(filepath,'*.h5'));
+    Filenames = {SelectedFolderInfo.name}';
+    [Filenames, FileGroup, FileNo] = ObtainFilenames(Filenames, filelocator);
 
-        if numel(Filenames) > 0
-            app.SampleName.Items = Filenames;
+    if numel(Filenames) > 0
+        % get user selected live file
+        app.SampleName.Items = Filenames;
+        % fine file group index for selected file
+        FileID = strcmp(Filenames, app.SampleName.Value);
 
-            %             [app,Data, Report] = ProcessH5File(app, filepath,Filenames{i}, Report, i, FileGroup);
+        % process selected file
+        [app, Data] = Live_ProcessH5File(app, filepath, app.SampleName.Value, FileID, FileGroup);
 
-        else
-            status = false;
-        end
-
+        % update live plots
+        UpdateLivePlots(app,Data)
     else
-        % if file processing fails
         status = false;
     end
-        pause(10)
+
+else
+    % if file processing fails
+    status = false;
 end
+
 
 
 end
